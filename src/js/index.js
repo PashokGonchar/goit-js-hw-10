7aed5e79e8a8a6f25ab07a479de53c3cf01ebfef
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-06-15 23:57:24 +0300

    create_new_element

--- src/js/index.js
+++ src/js/index.js
@@ -5,27 +5,24 @@ const selectEl = document.querySelector('.breed-select');
 const infoEl = document.querySelector('.cat-info');
 
 let breedsList = '';
-let breedName = ''
+let nameCat = ''
 
 fetchBreeds().then(data => {
-  console.log(data)
+  // console.log(data)
   data.forEach(el => {
     breedsList += `<option value="${el.id}">${el.name}-${el.id}</option>`;
   });
   selectEl.insertAdjacentHTML('afterbegin', breedsList);
 });
 
-const fetchCat = breedId => {
-  console.log(breedId)
+const fetchCat = (e) => {
+  console.log(e.target.value);
   fetchCatByBreed().then(data => {
-    console.log(data);
-    data.forEach(el => {
-breedName += `${el.id}`
-    });
-    infoEl.innerHTML= fetchCat(e.target.value);
+    data.forEach(e => {
+      nameCat += `<p>"${e.id}"</p>`;
+    })
   });
+  infoEl.insertAdjacentHTML('afterbegin', nameCat)
 };
-
-
-
+  
 selectEl.addEventListener('change', fetchCat);
